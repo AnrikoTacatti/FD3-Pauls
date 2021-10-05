4222fd2980881ce75a3b7002a8252b87b9db453f
Genrich Pauls <anriko.tacatti@gmail.com>
2021-10-05 19:29:15 +0300

    DoubleButton поправил

--- DoubleButton/src/Components/withRainbowFrame.js
+++ DoubleButton/src/Components/withRainbowFrame.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import RainbowFrame from './RainbowFrame.js';
 
 export default function withRainbowFrame(colors) {
     return function FramedDoubleButtonfinction(Db) {
         debugger;
-        return (props) => (
+        return (props) => {
+            let colorRainbow;
             <React.Fragment>
-                <RainbowFrame colors={colors}>  {props.children} </RainbowFrame>
-                <br />
-                <br />
-                <Db caption1={props.caption1} caption2={props.caption2} cbPressed={props.cbPressed}>
-                    {props.children}
-                </Db>
+                {
+                    colors.forEach((el, index) => {
+                        return colorRainbow = <div className="color-frame" style={{ border: "solid 10px " + el, padding: "10px" }}>
+                            {index === 0 ? <Db caption1={props.caption1} caption2={props.caption2} cbPressed={props.cbPressed}>{props.children}</Db> : colorRainbow}
+                        </div>
+                    })
+
+
+                }
             </React.Fragment>
-        )
+            return colorRainbow;
+        }
     }
 }
