a2aa4ca0e99f999393d20fab3ccfeba5eeaa1f77
Genrich Pauls <anriko.tacatti@gmail.com>
2021-11-06 21:20:20 +0300

    поправил publicPath: "./"

--- keep/webpack.config.js
+++ keep/webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './public/'),
         filename: "bundle.js",
-        publicPath: "/"
+        publicPath: "./"
 
     },
     devtool: 'source-map',
